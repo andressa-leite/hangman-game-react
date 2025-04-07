68c1c7b292aede55f906d12bf98e191799951c98
Andressa Leite <dre_tl@hotmail.com>
2025-04-07 19:48:09 -0400

    added 2 usestate variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+import wordLists from "./wordList.json";
+
 function App() {
+
+  // Randomize words
+  const [wordToTrack, setWordToTrack] = useState(() => {
+    return wordLists[Math.floor(Math.random() * wordLists.length)]
+  })
+
+  // Check randomized words
+  const [trackedWord, setTrackedWord] = useState<string[]>([])
+
   return (
     <>
-      <div>
+      <div
+        style={{
+          maxWidth: "800px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "5rem",
+          margin: "0 auto",
+          alignItems: "center",
+        }}
+      >
         <h1>Hello from React!</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
